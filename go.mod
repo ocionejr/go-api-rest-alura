15340356bd1cd489bff2eaf2413d1ce446155758
ocionejr <ocionejunior@gmail.com>
2022-07-26 14:38:46 -0300

    feat: docker and database connection

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ocionejr/go-api-rest-alura
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,14 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/ocionejr/go-api-rest-alura/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
